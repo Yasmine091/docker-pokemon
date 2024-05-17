2e8b9a818b0d02437ae4749fe565e9bb8faae4d9
Yasmine091 <yasmine.emdounan@gmail.com>
2024-05-17 17:22:02 +0200

    1er commit

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Aller dans le dossier front-end et installer les dépendances
+echo "Installation des dépendances pour le front-end..."
+cd front
+npm install
+
+# Construire le front-end React
+echo "Construction du front-end React..."
+npm run build
+
+# Revenir au dossier racine
+cd ..
+
+# Aller dans le dossier back-end et installer les dépendances
+echo "Installation des dépendances pour le back-end..."
+cd back
+npm install
+
+# Revenir au dossier racine
+cd ..
+
+# Construire et démarrer les conteneurs Docker en arrière-plan
+echo "Construction et démarrage des conteneurs Docker..."
+docker-compose up --build -d
+
+echo "Script de déploiement exécuté avec succès."
